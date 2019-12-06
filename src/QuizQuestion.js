d5f0d003b9577a604395cfb1fb1acedf76d6e74b
Jon Stout <jon-gh@abovecs.com>
2019-12-05 19:28:58 -0700

    passing test:module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { incorrectAnswer: false };
+    }
+
     handleClick(buttonText) {
-        if(buttonText === this.props.quiz_question.answer)  this.props.showNextQuestionHandler();
+        if(buttonText === this.props.quiz_question.answer) {
+            this.setState({incorrectAnswer: false});
+            this.props.showNextQuestionHandler();
+        } else {
+            this.setState({incorrectAnswer: true});
+        }
     }
 
     render() {
@@ -21,9 +31,9 @@ class QuizQuestion extends Component {
                                 )
                             })
                         }
-                        {/* <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} /> */}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main>
         );
     }
